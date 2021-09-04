c9d358d96da0b74be7aa35e8aee925952f599a5b
TheBoringDude <iamcoderx@gmail.com>
2021-09-04 14:02:08 +0800

    wait until networkidle

--- api/get.js
+++ api/get.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
             browser = await playwright.launchChromium({ headless: true })
             const context = await browser.newContext()
             const page = await context.newPage()
-            await page.goto(query.url)
+            await page.goto(query.url, {waitUntil: "networkidle"})
             const screenshot = await page.screenshot({ type: "png" })
             res.setHeader("Content-Type", "image/png")
             res.status(200).send(screenshot)
